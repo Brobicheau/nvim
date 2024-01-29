7456d0c5ee8452d82f13f47210f490080a8378d0
Bryan Robicheau <bryan.robicheau@parexel.com>
2024-01-29 13:12:39 -0500

    stuff

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,10 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 --
-vim.keymap.set("n", "<leader>d", "<leader>dzz", { silent = true })
-vim.keymap.set("n", "<leader>u", "<leader>uzz", { silent = true })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { silent = true })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { silent = true })
+vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<leader>fx", ":!chmod +x %<CR>", { silent = true })
+vim.keymap.set("n", "<C-f>", "!tmux neww tmux-sessionizer<CR>", { silent = true })
